fb98eef271220b8c60ac77ac7e3a6914ed08a623
mishael-codes <enyimishael22@gmail.com>
2023-08-11 17:08:53 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl font-bold text-red-500'>Hello TailwindCSS with React + Vite</h1>
+    </>
+  )
+}
+
+export default App
