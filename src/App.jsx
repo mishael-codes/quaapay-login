fad0b44653643d04998e97619789a41ff00fcfce
mishael-codes <enyimishael22@gmail.com>
2023-08-12 13:47:13 +0100

    design updates
    - added new icons to be used
    - completed the other half of the page
    - made it more responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,71 @@
 import "./App.css";
+import heroImage from "./assets/hero.svg";
+import appleLogo from "./assets/apple-store.svg";
+import googleLogo from "./assets/google-logo.svg";
+import googleStore from "./assets/google-play.svg";
+import slideInfo from "./assets/slide-info.svg";
+import tickIcon from "./assets/tick-icon.svg";
 
 function App() {
   return (
     <>
-      <section className="flex w-full h-screen bg-white">
-        <div className="w-1/2 h-full bg-quaapay-indigo"></div>
-        <div className="w-1/2">
-          <div className="w-80 h-[450px] ml-11 mt-24">
-            <div className="text-left">
-              <h1 className="text-black font-bold leading-[40px] text-3xl">
-                Welcome Back!
+      <section className="flex w-screen h-screen bg-white overflow-hidden">
+        <div className="w-1/2 h-full bg-quaapay-indigo flex items-center justify-center flex-col">
+          <img src={heroImage} alt="" className="scale-[.65]" />
+          <div>
+            <div className="text-left w-96 mx-0 my-auto text-white">
+              <h1 className="font-medium text-xl mb-1.5 -mt-7">
+                Introducing you to Quaapay Wallet
               </h1>
+              <p className="font-normal text-xs">
+                Downloading our mobile app, unlocks a world of possibilities for
+                managing your finances, including seamless currency exchange,
+                both locally and internationally.
+              </p>
+            </div>
+            <div className="w-full flex items-center justify-between mt-6">
+              <a
+                href="#"
+                className="w-5/12 bg-white flex items-center justify-center rounded-[10px]"
+              >
+                <img
+                  width="30px"
+                  height="30px"
+                  src={appleLogo}
+                  alt="apple-logo"
+                />
+                <button className="text-left ml-1.5 text-xs p-2">
+                  Download on <br />
+                  <span className="font-bold text-black">Apple Store</span>
+                </button>
+              </a>
+              <a
+                href="#"
+                className="w-5/12 bg-white flex items-center justify-center rounded-[10px]"
+              >
+                <img
+                  width="30px"
+                  height="30px"
+                  src={googleStore}
+                  alt="play-store"
+                />
+                <button className="text-left ml-1.5 text-xs p-2">
+                  Get on <br />
+                  <span className="font-bold text-black">Google Play</span>
+                </button>
+              </a>
+            </div>
+            <div>
+              <img src={slideInfo} alt="" className="mt-8 mb-4" />
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2 h-full flex justify-center flex-col ">
+          <div className="lg:w-80 lg:ml-12 md:w-72 md:h-auto md:ml-7">
+            <div className="text-left">
+              <h2 className="text-black font-bold leading-[40px] text-3xl">
+                Welcome Back!
+              </h2>
               <p className="text-sm">
                 Input your details or use Quaapay wallet details to login to
                 your account.
@@ -41,7 +96,12 @@ function App() {
               </label>
               <div className="w-full flex justify-between">
                 <label htmlFor="remember" className="text-sm">
-                  <input type="checkbox" name="remember" id="remember" />{" "}
+                  <input
+                    type="checkbox"
+                    name="remember"
+                    id="remember"
+                    className="appearance-none border border-quaapay-indigo checked:bg-quaapay-indigo w-5 h-5 after:content-['✔'] after:text-white after:flex after:items-center after:justify-center"
+                  />{" "}
                   Remember Me
                 </label>
 
@@ -61,9 +121,9 @@ function App() {
               <br />
               <button className="w-full border rounded mt-5 h-10 text-black flex items-center justify-center">
                 <img
-                  width="20"
-                  height="20"
-                  src="https://img.icons8.com/color/20/google-logo.png"
+                  width="30"
+                  height="30"
+                  src={googleLogo}
                   alt="google-logo"
                   className="mr-2"
                 />{" "}
